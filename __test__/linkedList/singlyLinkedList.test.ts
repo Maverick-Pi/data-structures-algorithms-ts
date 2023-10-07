f1383080fe2ed2140252ff3781e7f7d4fcc30ac2
maverick_pi <pimingjie1@outlook.com>
2023-10-07 18:04:16 +0800

    带哨兵的单链表

--- __test__/linkedList/singlyLinkedList.test.ts
+++ __test__/linkedList/singlyLinkedList.test.ts
@@ -20,9 +20,9 @@ describe('Singly Linked List', () => {
     list.addFirst(2)
     list.addFirst(3)
 
-    expect(list.head?.value).toBe(3)
-    expect(list.head?.next?.value).toBe(2)
-    expect(list.head?.next?.next?.value).toBe(1)
+    expect(list.head?.next?.value).toBe(3)
+    expect(list.head?.next?.next?.value).toBe(2)
+    expect(list.head?.next?.next?.next?.value).toBe(1)
   })
 
   it('should traverse the list using while loop and execute callback', () => {
@@ -63,8 +63,92 @@ describe('Singly Linked List', () => {
     list.addLast(2)
     list.addLast(3)
 
-    expect(list.head?.value).toBe(1)
-    expect(list.head?.next?.value).toBe(2)
-    expect(list.head?.next?.next?.value).toBe(3)
+    expect(list.head?.next?.value).toBe(1)
+    expect(list.head?.next?.next?.value).toBe(2)
+    expect(list.head?.next?.next?.next?.value).toBe(3)
+  })
+
+  it('should get the value by index of the list', () => {
+    list.addLast(1)
+    list.addLast(2)
+    list.addLast(3)
+
+    // 正常找到
+    expect(list.get(0)).toBe(1)
+    expect(list.get(1)).toBe(2)
+    expect(list.get(2)).toBe(3)
+
+    // 非正常找不到
+    expect(() => list.get(-2)).toThrowError('非法参数！')
+    expect(() => list.get(3)).toThrowError('非法参数！')
+  })
+
+  it('should insert a new node into the index position', () => {
+    list.insert(0, 1)
+    expect(list.head?.next?.value).toBe(1)
+    list.addLast(2)
+    list.addLast(3)
+    list.addLast(4)
+
+    list.insert(2, 9)
+    expect(list.get(2)).toBe(9)
+
+    list.insert(5, 5)
+    expect(list.get(5)).toBe(5)
+
+    // 找不到
+    expect(() => {
+      list.insert(7, 6)
+    }).toThrowError('该位置不存在！')
+  })
+
+  it('should remove the first node from the list', () => {
+    // 空链表
+    expect(() => {
+      list.removeFirst()
+    }).toThrowError('链表为空！')
+
+    // 非空链表
+    list.addLast(1)
+    list.addLast(2)
+    list.addLast(3)
+    list.addLast(4)
+    list.addLast(5)
+
+    list.removeFirst()
+    const result: number[] = []
+    list.traversalFor((value) => {
+      result.push(value)
+    })
+    expect(result).toEqual([2, 3, 4, 5])
+  })
+
+  it('should remove the node by index from the list', () => {
+    // 删除空链表
+    expect(() => {
+      list.remove(0)
+    }).toThrowError('该结点不存在！')
+
+    expect(() => {
+      list.remove(3)
+    }).toThrowError('该结点不存在！')
+
+    // 删除存在元素
+    list.addLast(1)
+    list.addLast(2)
+    list.addLast(3)
+    list.addLast(4)
+    list.addLast(5)
+
+    list.remove(2)
+
+    expect(list.head?.next?.value).toBe(1)
+    expect(list.head?.next?.next?.value).toBe(2)
+    expect(list.head?.next?.next?.next?.value).toBe(4)
+    expect(list.head?.next?.next?.next?.next?.value).toBe(5)
+
+    expect(() => {
+      list.remove(4)
+    }).toThrowError('该结点不存在！')
   })
 })
--- src/linkedList/singlyLinkedList.ts
+++ src/linkedList/singlyLinkedList.ts
@@ -1,6 +1,6 @@
 /**************************************************************************/ /**
  * @file src\linkedList\singlyLinkedList.ts
- * @desc 单向链表
+ * @desc 带哨兵的单向链表
  *
  * @author Mingjie Pi
  * @date 2023-10-06 14:36:11
@@ -23,15 +23,14 @@ class LinkedListNode<T> {
  * 单向链表类
  */
 export class SinglyLinkedList<T> {
-  head: LinkedListNode<T> | null = null
+  head: LinkedListNode<T> = new LinkedListNode<T>(null, null)
 
   /**
    * 在头部添加新结点
    * @param value 结点值
    */
   addFirst(value: T): void {
-    // 链表非空 或 空
-    this.head = new LinkedListNode(value, this.head)
+    this.insert(0, value)
   }
 
   /**
@@ -39,7 +38,7 @@ export class SinglyLinkedList<T> {
    * @param consumer 回调函数
    */
   traversalWhile(consumer: (value: T) => void): void {
-    let p = this.head
+    let p = this.head.next
     while (p !== null) {
       consumer(p.value!)
       p = p.next
@@ -51,7 +50,7 @@ export class SinglyLinkedList<T> {
    * @param consumer 回调函数
    */
   traversalFor(consumer: (value: T) => void): void {
-    for (let p = this.head; p !== null; p = p.next) {
+    for (let p = this.head.next; p !== null; p = p.next) {
       consumer(p.value!)
     }
   }
@@ -61,9 +60,6 @@ export class SinglyLinkedList<T> {
    * @returns 尾结点 或 null
    */
   private findLast(): LinkedListNode<T> | null {
-    // 空链表
-    if (this.head === null) return null
-    // 非空链表
     let p = this.head
     while (p.next !== null) {
       p = p.next
@@ -77,12 +73,78 @@ export class SinglyLinkedList<T> {
    */
   addLast(value: T): void {
     const last = this.findLast()
-    // 空链表
-    if (last === null) {
-      this.addFirst(value)
-    } else {
-      // 非空链表
-      last.next = new LinkedListNode<T>(value, null)
+    // 非空链表
+    last!.next = new LinkedListNode<T>(value, null)
+  }
+
+  /**
+   * 根据索引查找对应结点
+   * @param index 索引
+   * @returns 返回结点对象 或 null
+   */
+  private findNode(index: number): LinkedListNode<T> | null {
+    for (let p: LinkedListNode<T> | null = this.head, i = -1; p !== null; p = p.next, i++) {
+      if (i === index) {
+        return p
+      }
     }
+    return null // 没找到
+  }
+
+  /**
+   * 根据索引查找对应结点的值
+   * @param index 索引
+   * @returns 对应结点的值
+   */
+  get(index: number): T {
+    const node = this.findNode(index)
+    if (node === null) {
+      // 没找到抛异常
+      throw new Error('非法参数！')
+    }
+    // 找到了则返回对应结点的值
+    return node.value!
+  }
+
+  /**
+   * 向索引位置插入新结点
+   * @param index 索引
+   * @param value 插入结点的值
+   */
+  insert(index: number, value: T): void {
+    // 找到上一个结点
+    const pre = this.findNode(index - 1)
+    // 创建新结点
+    const newNode = new LinkedListNode<T>(value, this.findNode(index))
+    // 找不到，抛异常
+    if (pre === null) {
+      throw new Error('该位置不存在！')
+    }
+    // 更改上一个结点的指向
+    pre.next = newNode
+  }
+
+  /**
+   * 删除链表头结点
+   */
+  removeFirst(): void {
+    if (this.head.next === null) {
+      throw new Error('链表为空！')
+    }
+    this.head.next = this.head.next.next
+  }
+
+  /**
+   * 根据索引删除对应结点
+   * @param index 需要删除结点的索引
+   */
+  remove(index: number): void {
+    // 找到上一个结点
+    const pre = this.findNode(index - 1)
+    if (pre?.next === null || pre === null) {
+      throw new Error('该结点不存在！')
+    }
+    // 将上一个结点指向下一个结点
+    pre.next = pre.next.next
   }
 }
